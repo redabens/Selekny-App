f64140ab15a0dceaaab61cd599496c5eaba5c07a
RayaneTch <mr_taouache@esi.dz>
2024-04-01 20:00:47 +0100

    link front and back

--- android/app/src/main/kotlin/com/example/reda/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/reda/MainActivity.kt
@@ -2,9 +2,4 @@ package com.example.reda
 
 import io.flutter.embedding.android.FlutterActivity
 
-<<<<<<< HEAD
-class MainActivity: FlutterActivity() {
-}
-=======
 class MainActivity: FlutterActivity()
->>>>>>> main
