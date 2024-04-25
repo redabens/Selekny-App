f13c048106d2ed372b710f24f06ca5b17f63724a
remohda <redabensredabens49@gmail.com>
2024-04-25 14:14:43 +0200

    last update

--- lib/Logic/Cubit/detailsDemandeCubit.dart
+++ lib/Logic/Cubit/detailsDemandeCubit.dart
@@ -3,6 +3,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 part 'detailsDemandeCubit_State.dart';
 
 class DetailsDemandeCubit extends Cubit<DetailsDemandeCubitState>{
-  DetailsDemandeCubit(super.initialState)
+  DetailsDemandeCubit(super.initialState);
 
 }
