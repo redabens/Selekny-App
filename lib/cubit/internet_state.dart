269cb7d8954a24d0214b2ff516b711b1760bf0b2
RayaneTch <mr_taouache@esi.dz>
2024-05-03 15:40:17 +0100

    modif recherche et notif

--- lib/cubit/internet_state.dart
+++
@@ -1,8 +0,0 @@
-part of 'internet_cubit.dart';
-
-enum ConnectivityStatus { connected, disconnected }
-
-class InternetStatus {
-  final ConnectivityStatus status;
-  const InternetStatus(this.status);
-}
